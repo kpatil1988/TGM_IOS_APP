1f68fd1947e4792c3a6170fdb3967090468c578b
kpatil1988 <karthikpatil@live.com>
2024-12-25 06:17:16 +0530

    Initial Commit

---
+++ GoldenMinds/GoldenMindsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoldenMindsApp.swift
+//  GoldenMinds
+//
+//  Created by Karthik patil on 25/12/24.
+//
+
+import SwiftUI
+
+@main
+struct GoldenMindsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
